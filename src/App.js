4089484b56b3b431426c182ea441259f3606e3fb
devmaxwel <eng.maxwel.ochieng@gmail.com>
2022-01-27 15:29:11 +0300

    Chart-Js revisitting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Barchart from './components/Barchart';
+import Linechart from './components/LineChart';
+import Piechart from './components/PieChart';
+import { UserData } from './data/Data'
 
 function App() {
+
+  const [userData, setUserData]=useState({
+    labels:UserData.map((data) => {
+      return data.year
+    }),
+
+    datasets:[{
+      label:"Users Gained",
+      data:UserData.map((data) => {
+        return data.userGain
+      }),
+      backgroundColor:['#6065AD', '#0E6EE7', '#AB5498'],
+      borderWidth:2,
+      borderColor:"black"
+    }]
+  });
+
+useEffect(() => {
+  return userData;
+
+},[UserData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display:'flex'
+    }}>
+      <div style={{
+        width:500
+      }}> 
+         <Barchart chartData = {userData} />
+      </div>
+
+      <div style={{
+        width:300
+      }}> 
+         <Piechart chartData = {userData} />
+      </div>
+
+      <div style={{
+        width:500
+      }}> 
+         <Linechart chartData = {userData} />
+      </div>
     </div>
   );
 }
